bc1a269d34e5c0864e009a745393fe6b8da0f0a8
Jevodan <loisapandohva@gmail.com>
2023-11-19 18:08:01 +0300

    initial

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ func main() {
 	logrus.SetFormatter(&logrus.JSONFormatter{})
 	logrus.Infof("User: %s", getUsername())
 	fmt.Println("sum 2 and 3 is", sum(2, 3))
-
 	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		fmt.Fprintf(w, "Service: module 39\nStatus: OK\n")
